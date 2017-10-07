59ed160755a784b9e7fdc07080eb4bbbcc5c4f47
Shaik Adil <shaikadil86@gmail.com>
2017-10-07 20:43:30 +0530

    Init Chatbot

---
+++ index.js
@@ -0,0 +1,29 @@
+const express =require('express');
+const bodyparser =require('body-parser');
+const app=express();
+
+app.set('port',(process.env.PORT || 5000));
+
+//Allow us to process the data
+app.use(bodyparser.urlencoded({extended:false}));
+app.use(bodyparser.json());
+//Routes
+app.get('/',function (req,res) {
+    res.send("Hi I am a chatbot")
+    
+});
+
+//Facebook
+app.get('/webhook/', function (req,res) {
+    if(req.query['hub.verifytoken']==="adilshaik")
+    {res.send(req.query['hub.challenge'])}
+    else res.send("Wrong token")
+    
+});
+
+app.listen(app.get('port'),function () {
+    console.log("running port")
+    
+});
+
+
