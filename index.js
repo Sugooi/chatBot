fa156fa17a3d4ff988e50bc7bb25988fcc6be72a
Shaik Adil <shaikadil86@gmail.com>
2017-10-08 12:45:07 +0530

    added insent

--- index.js
+++ index.js
@@ -93,7 +93,7 @@ function decideMessage(sender, text1) {
         sendButtonMessage(sender,"What season do you like?")}
     else if(inSentence(nude,text)){sendText(sender,"This is the most naked form of mine. ;)")
         sendImageMessage(sender)}
-    else if(inSentence("menu",text)){menu(sender,text)}
+    else if(inSentence("menu",text)){menu(sender,"Menu:")}
 
     else {sendText(sender, "Wow, you just said \"" + text.substring(0, 100)+"\"")
     }
